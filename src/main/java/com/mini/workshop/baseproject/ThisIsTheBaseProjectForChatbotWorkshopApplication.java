e748f012c4294911895b67c9eb12afaf9517732c
Suvidha <Suvidha@192.168.1.6>
2020-08-09 10:27:21 +0530

    Base Project first commit

---
+++ src/main/java/com/mini/workshop/baseproject/ThisIsTheBaseProjectForChatbotWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mini.workshop.baseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisIsTheBaseProjectForChatbotWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisIsTheBaseProjectForChatbotWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mini/workshop/baseproject/ThisIsTheBaseProjectForChatbotWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.workshop.baseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisIsTheBaseProjectForChatbotWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
